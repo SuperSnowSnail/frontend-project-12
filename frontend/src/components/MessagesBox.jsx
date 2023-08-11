85fa3436c2552efd2d3af7ea22bb765e07efcb6f
SnowSnail <f2961@yandex.ru>
2023-08-11 19:06:31 +0300

    Add scroll to botton in the message box

--- frontend/src/components/MessagesBox.jsx
+++ frontend/src/components/MessagesBox.jsx
@@ -1,3 +1,4 @@
+import ScrollToBottom from 'react-scroll-to-bottom';
 import { useSelector } from 'react-redux';
 
 import messagesSelectors from '../selectors/messagesSelectors';
@@ -6,14 +7,18 @@ const MessagesBox = () => {
   const messages = useSelector(messagesSelectors.selectCurrentChannelMessages);
 
   return (
-    <div className="chat-messages overflow-auto px-5" id="messages-box">
+    <ScrollToBottom
+      className="chat-messages overflow-auto px-5"
+      id="messages-box"
+      followButtonClassName="follow-button"
+    >
       {messages.map(({ body, username, id }) => (
         <div key={id} className="text-break mb-2">
           <b>{username}</b>
           {`: ${body}`}
         </div>
       ))}
-    </div>
+    </ScrollToBottom>
   );
 };
 
